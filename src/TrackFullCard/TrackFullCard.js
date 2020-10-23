5731dee073dbe3d43b696836ff964a0ee66e0d34
Laura Sikora <lauraannsikora@gmail.com>
2020-10-23 15:32:05 -0400

    updated trackFullCard

--- src/TrackFullCard/TrackFullCard.js
+++ src/TrackFullCard/TrackFullCard.js
@@ -10,17 +10,9 @@ export default class TrackFullCard extends React.Component {
     state = {
         hikeSection: '',
         notes: '',
-        'grounding-track': false,
+        'warmup-track': false,
         'cooling-track': false,
-        'heat-rising-track': false,
-        'energizing-track': false,
-        'strengthening-track': false,
-        'relaxing-track': false,
-        'releasing-pressure track': false,
-        'stabilizing-track': false,
-        'increasing-flexibility': false,
     }
-
 
     componentDidMount = () => {
         const { track_id } = this.props.match.params;
@@ -149,62 +141,14 @@ export default class TrackFullCard extends React.Component {
                 </div>
                 
                 <form onSubmit={this.handleSubmit}>
-                    <h3 className='title' >Pick attributes:</h3>
-                    <div className='attributes-container'>
-                        <div className='row1'>
-                            <input className='input' type='checkbox' name='grounding-track' onClick={this.handleAddAttribute} />
-                            <label>Grounding track</label>
-                            <br />
-
-                            <input className='input' type='checkbox' name='cooling-track' onClick={this.handleAddAttribute} />
-                            <label>Cooling track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='heat-rising-track' onClick={this.handleAddAttribute} />
-                            <label>Heat rising track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='energizing-track' onClick={this.handleAddAttribute} />
-                            <label>Energizing track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='strengthening-track' onClick={this.handleAddAttribute} />
-                            <label>Strengthening track</label>
-
-                        </div>
-                        <div className='row2'>
-                            <input className='input' type='checkbox' name='relaxing-track' onClick={this.handleAddAttribute} />
-                            <label>Relaxing track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='releasing-track' onClick={this.handleAddAttribute} />
-                            <label>Releasing preassure track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='stabilizing-track' onClick={this.handleAddAttribute} />
-                            <label>Stabilizing track</label>
-
-                            <br />
-                            <input className='input' type='checkbox' name='flexibility-track' onClick={this.handleAddAttribute} />
-                            <label>Flexcibility building track</label>
-                        </div>
-                    </div>
                     <div className='note-container'>
                         <h3 className='title' >Notes</h3>
                         <textarea rows="10" cols='50' onChange={this.handleNotes} value={this.state.notes}></textarea>
                     </div>
-                    <div className='options-container'>
-                        <select className='form__field2' name='hike-menu' onChange={this.handleSaveTrackAs} required >
-                            <option value='' required >Save to my hike as:</option>
-                            <option value='1'>warm up track</option>
-                            <option value='3' >break track</option>
-                            <option value='4'>peak track</option>
-                            <option value='2'> mid-hike track</option>
-                            <option value='5' > after-peak stabilizing track</option>
-                        </select>
+                   
                         {this.state.hikeSection !== '' ? <button type='submit' >Add to hike</button> : null}
                         <button onClick={this.handleBackButton} >Back</button>
-                    </div>
+
                 </form>
             </div>
         );
