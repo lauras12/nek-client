7627c91c1672436f04f2c99c17e9ef60be021d49
Laura Sikora <lauraannsikora@gmail.com>
2020-11-23 18:18:29 -0400

    updated API_HIkes_SErvice

--- src/services/API_Hike_service.js
+++ src/services/API_Hike_service.js
@@ -96,7 +96,7 @@ const APIHikeCalls = {
             .then(res => res.json());
     },
 
-    getAllTracksInHike: (hikeId) => {
+    getAllTrackInHike: (hikeId) => {
         const URL = config.API_ENDPOINT + `/hikes/${hikeId}`;
         return fetch(URL, {
                 headers: {
