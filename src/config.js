09baf17e32c99b20ca86f363d60600486de33ada
Laura Sikora <lauraannsikora@gmail.com>
2021-01-11 17:27:04 -0400

    updated tests

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-import env from "react-dotenv";
+// import env from "react-dotenv";
 export default {
    // API_ENDPOINT: 'http://localhost:8000/api',
-    API_ENDPOINT: (env && env.API_URL) || 'https://nek-api.herokuapp.com/api',
+    API_ENDPOINT: (window.env && window.env.API_URL) || 'https://nek-api.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY,
 }
